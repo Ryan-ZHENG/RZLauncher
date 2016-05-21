7cac711b38f7ce1b0614793bbce0e818ff6e6de2
zheng_dp <zheng_dp@chinaxuhu.com>
2016-05-21 13:42:36 +0800

    init

---
+++ app/src/main/java/zheng/ryan/rzlauncher/activity/LauncherActivity.java
@@ -0,0 +1,16 @@
+package zheng.ryan.rzlauncher.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import zheng.ryan.rzlauncher.R;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
+
